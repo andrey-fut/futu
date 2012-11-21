6cc49dc9102f352dfa7e16af717d61dbcdac47c3
Maksimovich Andrey <andrey.55555@ya.ru>
2012-11-22 00:00:10 +0200

    Test_10 merge --on-ff

--- php_info.php
+++ php_info.php
@@ -28,6 +28,8 @@
 
 	// comment_9 branch common making head repository (home) test merge --no-ff
 	
+	// comment_10 branch common making head repository (home) test merge --no-ff	
+	
     phpinfo();
 	
 	
