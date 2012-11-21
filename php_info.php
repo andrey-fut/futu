9cb3b70a0b1769b10b7e03e9d4874afec09ded58
andrey <andrey@localhost.localdomain>
2012-11-21 09:26:33 +0200

    test branch commit_3 head (wokr)

--- php_info.php
+++ php_info.php
@@ -8,6 +8,8 @@
 	// comment_1 branch common-dev making head repository
 	
 	// comment_2 branch common-dev making head repository
+
+	// comment_3 branch common-dev making head repository (work)
 	
     phpinfo();
 	
