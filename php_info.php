c0ed5a1d96d0a04b1c012139eac93872607759f5
andrey <andrey@localhost.localdomain>
2012-11-21 09:38:12 +0200

    test branch commit_3 clone (work)

--- php_info.php
+++ php_info.php
@@ -8,6 +8,8 @@
 	// comment_1 branch common-dev making head repository
 	
 	// comment_2 branch common-dev making head repository
+
+	// comment_3 branch common-dev making clone repository (work)
 	
     phpinfo();
 	
